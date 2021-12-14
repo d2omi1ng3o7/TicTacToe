b09392bbe3dc60829aa7925c122f17d710a945f8
d2omi1ng3o7 <95539986+d2omi1ng3o7@users.noreply.github.com>
2021-12-14 17:44:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-# TicTacToe - version 1.52
+# TicTacToe - version 1.55
 
 from random import randrange
+import os
 
 def display_board(board): # rysuje plansze
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
     for i in range(3):
         print("+-------+-------+-------+", "|       |       |       |", sep="\n")
         for j in range(3):
@@ -47,7 +53,7 @@ def victory_for(board): # funkcja sprawdza kto wygrał
     # do poprawy sprawdzanie wygranego              
     if (board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X") or (board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X") or (board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X") or (board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X") or (board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X") or (board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X") or (board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X") or (board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X"):
         return "Przegrałeś...Nie dobrze..."
-    elif (board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O") or (board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O") or (board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O") or (board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O") or (board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O") or (board[0][0] == "O" and board[1][0] == "O" and board[1][0] == "O") or (board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O") or (board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X"):
+    elif (board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O") or (board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O") or (board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O") or (board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O") or (board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O") or (board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O") or (board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O") or (board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X"):
         return "Wygrałeś... Brawo!"
     elif boolean == True:
         return "No to mamy remis..." 
@@ -68,14 +74,16 @@ def draw_move(board): # ruch komputera
     return board
 
 board = [[1,2,3],[4,5,6],[7,8,9]]
-while True:
-    board = draw_move(board)
-    result = victory_for(board)
-    if result != 0:
-        print(result)
-        break
-    board = enter_move(board)
-    result = victory_for(board)
-    if result != 0:
-        print(result)
-        break
+
+if __name__ == '__main__':
+    while True:
+        board = draw_move(board)
+        result = victory_for(board)
+        if result != 0:
+            print(result)
+            break
+        board = enter_move(board)
+        result = victory_for(board)
+        if result != 0:
+            print(result)
+            break
