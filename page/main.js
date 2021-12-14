06ec60b380998c2b1c311ca49372ed1ec0299ee3
d2omi1ng3o7 <95539986+d2omi1ng3o7@users.noreply.github.com>
2021-12-14 18:56:33 +0100

    Add files via upload

---
+++ page/main.js
@@ -0,0 +1,27 @@
+document.getElementById("area1").addEventListener("click", function(){
+    document.getElementById("area1").innerText = "X";
+})
+document.getElementById("area2").addEventListener("click", function(){
+    document.getElementById("area2").innerText = "X";
+})
+document.getElementById("area3").addEventListener("click", function(){
+    document.getElementById("area3").innerText = "X";
+})
+document.getElementById("area4").addEventListener("click", function(){
+    document.getElementById("area4").innerText = "X";
+})
+document.getElementById("area5").addEventListener("click", function(){
+    document.getElementById("area5").innerText = "X";
+})
+document.getElementById("area6").addEventListener("click", function(){
+    document.getElementById("area6").innerText = "X";
+})
+document.getElementById("area7").addEventListener("click", function(){
+    document.getElementById("area7").innerText = "X";
+})
+document.getElementById("area8").addEventListener("click", function(){
+    document.getElementById("area8").innerText = "X";
+})
+document.getElementById("area9").addEventListener("click", function(){
+    document.getElementById("area9").innerText = "X";
+})
